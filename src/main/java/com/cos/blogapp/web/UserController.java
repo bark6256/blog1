f8ff74440a81813aef131fbf0b8d963565fb6549
unknown <bark6256@naver.com>
2021-09-29 11:49:16 +0900

    회원정보 수정 + 유효성, 인증, 권한 체크

--- src/main/java/com/cos/blogapp/web/UserController.java
+++ src/main/java/com/cos/blogapp/web/UserController.java
@@ -12,15 +12,20 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.cos.blogapp.domain.user.User;
 import com.cos.blogapp.domain.user.UserRepository;
+import com.cos.blogapp.handler.ex.MyAsyncNotFoundException;
 import com.cos.blogapp.util.MyAlgorithm;
 import com.cos.blogapp.util.SHA;
 import com.cos.blogapp.util.Script;
+import com.cos.blogapp.web.dto.CMRespDto;
 import com.cos.blogapp.web.dto.JoinReqDto;
 import com.cos.blogapp.web.dto.LoginReqDto;
+import com.cos.blogapp.web.dto.UserUpdateDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -30,9 +35,38 @@ public class UserController {
 	
 	private final UserRepository userRepository;
 	private final HttpSession session;
-		
-	//http://localhost:8080/login -> login.jsp
-	// views/user/login.jsp
+	
+	@PutMapping("/user/{id}")
+	public @ResponseBody CMRespDto<String> update(@PathVariable int id, @Valid @RequestBody UserUpdateDto dto,
+			BindingResult bindingResult) {
+		// 유효성
+		if (bindingResult.hasErrors()) {
+			Map<String, String> errorMap = new HashMap<>();
+			for (FieldError error : bindingResult.getFieldErrors()) {
+				errorMap.put(error.getField(), error.getDefaultMessage());
+			}
+			throw new MyAsyncNotFoundException(errorMap.toString());
+		}
+
+		// 인증
+		User principal = (User) session.getAttribute("principal");
+		if (principal == null) { // 로그인 안됨
+			throw new MyAsyncNotFoundException("인증이 되지 않았습니다.");
+		}
+
+		// 권한
+		if (principal.getId() != id) {
+			throw new MyAsyncNotFoundException("회원정보를 수정할 권한이 없습니다.");
+		}
+
+		// 핵심로직
+		principal.setEmail(dto.getEmail());
+		session.setAttribute("principal", principal); // 세션 값 변경
+
+		userRepository.save(principal);
+
+		return new CMRespDto<>(1, "성공", null);
+	}
 	
 	@GetMapping("/joinForm")         // --------------- 회원가입 --------------
 	public String joinForm() {
--- src/main/webapp/WEB-INF/views/user/updateForm.jsp
+++ src/main/webapp/WEB-INF/views/user/updateForm.jsp
@@ -4,18 +4,42 @@
 <%@ include file="../layout/header.jsp" %>
 
 <div class="container">
-	<form> 
+	<form onsubmit="update(event, ${sessionScope.principal.id})">
 		<div class="form-group">
 			<input type="text" value="${sessionScope.principal.username}" name="username" class="form-control" placeholder="Enter username" required maxlength="20" readonly>
 		</div>
 		<div class="form-group">
-			<input type="password" "${sessionScope.principal.password}"  name="password" class="form-control"	placeholder="Enter password" required maxlength="20">
+			<input type="password" value="${sessionScope.principal.password}"  name="password" class="form-control"	placeholder="Enter password" required maxlength="20">
 		</div>
 		<div class="form-group">
-			<input type="email" "${sessionScope.principal.email}"  name="email" class="form-control"	placeholder="Enter email" required maxlength="50">
+			<input type="email" id="email" value="${sessionScope.principal.email}"  name="email" class="form-control"	placeholder="Enter email" required maxlength="50">
 		</div>
 		<button type="submit" class="btn btn-primary">회원수정</button>
 	</form>
 </div>
+
+<script>
+async function update(event, id){ 
+	   event.preventDefault();
+	   let userUpdateDto = {
+			   email: document.querySelector("#email").value
+	   };
+		let response = await fetch("http://localhost:8080/user/"+id, {
+			method: "put",
+			body: JSON.stringify(userUpdateDto),
+			headers: {
+				"Content-Type": "application/json; charset=utf-8"
+			}
+		});
+		
+		let parseResponse = await response.json();
+		if(parseResponse.code == 1){
+			alert("업데이트 성공");
+			location.href = "/";
+		}else{
+			alert("업데이트 실패 : " + parseResponse.msg);
+		}
+}
+</script>
 
 <%@ include file="../layout/footer.jsp" %>
